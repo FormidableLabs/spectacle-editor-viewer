afef05b0cf5bf4b61b6a9737b011db6582bdd510
Ken Wheeler <ken_wheeler@me.com>
2016-07-19 09:14:41 -0700

    Adding viewer component

--- src/components/viewer.js
+++ src/components/viewer.js
@@ -1,14 +1,65 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
+import * as Core from 'spectacle';
+import theme from '../theme';
 
-export default class Viewer extends Component {
-  componentDidMount() {
-    // do stuff
-  }
-  render() {
-    return <p>Test</p>;
-  }
-}
+const { Spectacle, Deck, Slide, Appear } = Core;
+
+Core.Plotly = (props) => <iframe {...props} />;
+
+const renderChildren = (nodes) =>
+  nodes.map((node) => {
+    // Text node
+    if (typeof node === 'string') {
+      return node;
+    }
+
+    // defaultText handling
+    if (node.type === 'Text' && !node.children) {
+      return node.defaultText;
+    }
+
+    const { type, children, props } = node;
+
+    // Get component from Spectacle core
+    const Tag = Core[type];
+
+    // Wrap ListItems in Appear
+    if (node.type === 'ListItem') {
+      return (
+        <Appear>
+          <Tag key={node.id} {...props}>
+            {children && renderChildren(children)}
+          </Tag>
+        </Appear>
+      );
+    }
+
+    // Render and recurse
+    return (
+      <Tag key={node.id} {...props}>
+        {children && renderChildren(children)}
+      </Tag>
+    );
+  });
+
+const renderSlides = (slides) =>
+  slides.map((slide) => (
+    <Slide key={slide.id}>
+      {slide.children && renderChildren(slide.children)}
+    </Slide>
+  ));
+
+const Viewer = (props) => (
+  <Spectacle theme={{ screen: theme, print: theme }}>
+    <Deck transition={[]}>
+      {renderSlides(props.presentation.content.slides)}
+    </Deck>
+  </Spectacle>
+);
 
 Viewer.propTypes = {
   presentation: PropTypes.object,
 };
+
+export default Viewer;
+
