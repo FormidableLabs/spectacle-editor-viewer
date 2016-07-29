016f6c10c8ef61a636c40a31f8e872af5558e7ef
Rob Gerstenberger <rob.gerstenberger@formidable.com>
2016-07-29 11:24:13 -0700

    rearrange prop order

--- demo/index.js
+++ demo/index.js
@@ -4,6 +4,6 @@ import { render } from 'react-dom';
 import { Viewer } from '../src/';
 import json from './test.json';
 
-const Demo = () => <Viewer presentation={json} />;
+const Demo = () => <Viewer content={json} />;
 
 render(<Demo />, document.getElementById('root'));
